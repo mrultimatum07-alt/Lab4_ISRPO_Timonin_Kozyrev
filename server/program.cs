b7ca6093565b425654ffe4668c0ce00c2b44596a
mrultimatum07-alt <mrultimatum07@gmail.com>
2026-01-19 15:58:05 +0300

    Добавление группы и даты

--- server/program.cs
+++ server/program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Привет,нас зовут Тимонин Иван,Козырев Артемий");
+Console.WriteLine("Привет,нас зовут Тимонин Иван,Козырев Артемий мы из ИСП-233 сейчас 19.01.2026");
