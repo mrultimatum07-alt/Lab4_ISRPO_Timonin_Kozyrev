29ca3e7fae9af632f1c9b9636ba14a30ded17e5a
mrultimatum07-alt <mrultimatum07@gmail.com>
2026-01-19 16:17:14 +0300

    Создание менюс илюзией выбора

--- server/program.cs
+++ server/program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Привет,нас зовут Тимонин Иван,Козырев Артемий мы из ИСП-233 сейчас 19.01.2026");
+using System.Data;
+using System.Diagnostics;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+
+Console.WriteLine("Привет");
+
+while (true) {
+    Console.WriteLine("Что ты хочешь узнать 1 - Фио,2 - Группу,3 - Дату ,4 - выход");
+    string? a = Console.ReadLine();
+    if (a == "1")
+    {
+        Console.WriteLine("Тимонин Иван,Козырев Артемий");
+    } else if (a == "2")
+    {
+        Console.WriteLine("ИСП-233");
+    } else if(a == "3")
+    {
+        Console.WriteLine(DateTime.Now);
+    }
+    else
+    {
+        break;
+    }
+    
+}
